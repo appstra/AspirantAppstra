f0c2237cf45d3d2ce43c03dcc73d6e2a4891f30c
danielcontreras205 <danielcontreras205@gmail.com>
2024-12-31 17:31:22 -0500

    inicialicer

--- src/main/java/com/appstra/aspirante/AspiranteApplication.java
+++ src/main/java/com/appstra/aspirante/AspiranteApplication.java
@@ -2,9 +2,10 @@ package com.appstra.aspirante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AspiranteApplication {
+public class AspiranteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AspiranteApplication.class, args);
